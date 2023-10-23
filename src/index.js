8dfd99ece6d33622e4cb2d4fed15a2c55fd696c1
shoebilyas123 <shoebilyas123@gmail.com>
2023-10-23 11:44:58 +0530

    learn: getting started

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   const element = document.createElement('div');
 
+  // Lodash is now imported with script
   element.innerHTML = _.join(['hello', 'world'], '');
 
   return element;
