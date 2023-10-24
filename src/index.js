6d742fe4c969e931411afd9a9b52594bd3a55dfd
shoebilyas123 <shoebilyas123@gmail.com>
2023-10-24 11:28:04 +0530

    learn: asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
+import style from './style.css';
+import myImg from './myimg.jpg';
+import xmlData from './data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash is now imported with script
   element.innerHTML = _.join(['hello', 'world'], '');
+  element.classList.add('hello');
+
+  const Icon = new Image();
+  Icon.src = myImg;
+
+  element.appendChild(Icon);
+
+  console.log({ xmlData });
 
   return element;
 }
